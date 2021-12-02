2e189d1485fb5c092e8c4ad6d5ac18f0606c4559
Steffen Tømmerås <steffen.tommeras@gmail.com>
2021-12-02 12:40:59 +0100

    completed day1

--- day1/day1.py
+++ day1/day1.py
@@ -1,12 +1,35 @@
-input = open("day1-input.txt")
-nums = input.readlines()
+### Part one
 
-sum = 0
+with open("day1-input.txt") as f:
+    input = f.read().splitlines()
 
-# check for each element of nums if it's bigger than the previous element
-for i in range(1, len(nums)):
-    if (nums[i] > nums[i - 1]):
-        sum += 1
+depths = [int(n) for n in input]
 
-print(sum)
-# 1687, apparently wrong (too low) ...
+# check for each element of list if it's bigger than the previous element, and adds to num
+def check_increases(list):
+    sum = 0
+    for i in range(1, len(list)):
+        if list[i] > list[i - 1]:
+            sum += 1
+    return sum
+
+increases_depth = check_increases(depths)
+
+print(increases_depth)
+# 1688
+
+
+### Part two
+
+depth_windows = []
+
+# check if 3 numbers left, adds the next three together and adds to new list
+for i in range(0, len(depths)):
+    if (i + 2) == (len(depths)):
+        break
+    else:
+        depth_windows.append(depths[i] + depths[i + 1] + depths[i + 2])
+
+increases_depth_windows = check_increases(depth_windows)
+
+print(increases_depth_windows)
