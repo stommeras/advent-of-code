7696a0f25898f404feca6ad1cb37554b43d8389e
Steffen Tømmerås <steffen.tommeras@gmail.com>
2021-12-15 11:26:36 +0100

    complete day13

---
+++ 2021/day13/day13.py
@@ -0,0 +1,23 @@
+import numpy as np
+from parse import findall
+
+with open("day13-input.txt") as f:
+    input = f.read().strip()
+
+coords = findall('{:d},{:d}', input)
+folds = findall("{:l}={:d}", input)
+
+P = np.zeros((9999, 9999), bool)
+
+for x, y in coords:
+    P[y, x] = True
+
+for axis, a in folds:
+    if axis == "x":
+        P = P[:,:a] | P[:,2*a:a:-1]
+    if axis == "y":
+        P = P[:a,:] | P[2*a:a:-1,:]
+    print(P.sum())
+
+print(np.array2string(P, separator="",
+    formatter = {"bool":{0:" ",1:"█"}.get}))
