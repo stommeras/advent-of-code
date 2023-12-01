9aba12d13be6e74edd531ed69c5662409f4c1300
Steffen Tømmerås <steffen.tommeras@gmail.com>
2023-12-01 14:12:57 +0100

    complete day1 2023

---
+++ 2023/day1/day1.py
@@ -0,0 +1,28 @@
+### Part One
+import sys
+import re
+
+with open(sys.argv[1], "r") as f:
+    input = f.read().split("\n")
+
+# Part one
+
+sum1 = 0
+
+# for line in input:
+#     calibrationValue = re.sub(r'[^0-9]', '', line)
+#     sum1 += int(calibrationValue[0] + calibrationValue[-1])
+
+# print(sum1)
+
+
+# Part two
+
+sum2 = 0
+
+for line in input:
+    line = line.replace("one", "one1one").replace("two", "two2two").replace("three", "three3three").replace("four", "four4four").replace("five", "five5five").replace("six", "six6six").replace("seven", "seven7seven").replace("eight", "eight8eight").replace("nine", "nine9nine")
+    calibrationValue = re.sub(r'[^0-9]', '', line)
+    sum2 += int(calibrationValue[0] + calibrationValue[-1])
+
+print(sum2)
